fa492b1f7240cabb6f67761d001bdc81bd83a024
olehburkun <oleh.burkun@gmail.com>
2026-06-07 12:55:42 +0200

    round display with comets using struct

--- round_tft/round_tft.ino
+++ round_tft/round_tft.ino
@@ -8,28 +8,67 @@
 
 #define CENTER_X  120
 #define CENTER_Y  120
-#define ORBIT_R    90
-#define DOT_R       6
 #define TRAIL_LEN  12
 
 Adafruit_GC9A01A tft(TFT_CS, TFT_DC, TFT_RST);
 
-float angle = 0;
-int trailX[TRAIL_LEN];
-int trailY[TRAIL_LEN];
+struct Comet {
+  float angle;
+  float speed;
+  int orbitRadius;
+  int dotRadius;
+  uint8_t r, g, b;
+  int trailX[TRAIL_LEN];
+  int trailY[TRAIL_LEN];
+};
+
+Comet comets[2];
+
+void initComet(Comet &c, float angle, float speed, int orbitR, int dotR, uint8_t r, uint8_t g, uint8_t b) {
+  c.angle = angle;
+  c.speed = speed;
+  c.orbitRadius = orbitR;
+  c.dotRadius = dotR;
+  c.r = r;  c.g = g;  c.b = b;
+  for (int i = 0; i < TRAIL_LEN; i++) {
+    c.trailX[i] = CENTER_X + orbitR;
+    c.trailY[i] = CENTER_Y;
+  }
+}
+
+void updateComet(Comet &c) {
+  float rad = c.angle * PI / 180.0;
+  int x = CENTER_X + c.orbitRadius * cos(rad);
+  int y = CENTER_Y + c.orbitRadius * sin(rad);
+
+  tft.fillCircle(c.trailX[TRAIL_LEN - 1], c.trailY[TRAIL_LEN - 1], c.dotRadius + 1, GC9A01A_BLACK);
+
+  for (int i = TRAIL_LEN - 1; i > 0; i--) {
+    c.trailX[i] = c.trailX[i - 1];
+    c.trailY[i] = c.trailY[i - 1];
+  }
+  c.trailX[0] = x;
+  c.trailY[0] = y;
+
+  for (int i = TRAIL_LEN - 1; i >= 0; i--) {
+    float fade = (float)(TRAIL_LEN - i) / TRAIL_LEN;
+    uint16_t color = tft.color565(c.r * fade, c.g * fade, c.b * fade);
+    tft.fillCircle(c.trailX[i], c.trailY[i], c.dotRadius, color);
+  }
+
+  c.angle += c.speed;
+  if (c.angle >= 360) c.angle = 0;
+}
 
 void drawTwoLineCentered(const char* line1, const char* line2, uint16_t color) {
   int16_t x1, y1;
   uint16_t w1, h1, w2, h2;
   tft.setTextColor(color);
-
   tft.getTextBounds(line1, 0, 0, &x1, &y1, &w1, &h1);
   tft.getTextBounds(line2, 0, 0, &x1, &y1, &w2, &h2);
-
   int gap = 6;
   int totalH = h1 + gap + h2;
   int startY = (240 - totalH) / 2;
-
   tft.setCursor((240 - w1) / 2, startY);
   tft.print(line1);
   tft.setCursor((240 - w2) / 2, startY + h1 + gap);
@@ -38,45 +77,18 @@ void drawTwoLineCentered(const char* line1, const char* line2, uint16_t color) {
 
 void setup() {
   tft.begin();
+  tft.setRotation(2);
   tft.fillScreen(GC9A01A_BLACK);
 
-  // initialize all trail positions to the starting dot position
-  for (int i = 0; i < TRAIL_LEN; i++) {
-    trailX[i] = CENTER_X + ORBIT_R;
-    trailY[i] = CENTER_Y;
-  }
+  initComet(comets[0],   0, 3, 90, 6, 255,   0,   0);  // red, outer orbit
+  initComet(comets[1], 180, 2, 65, 4,   0,   0, 255);  // blue, inner orbit
 
   tft.setTextSize(2);
   drawTwoLineCentered("BurCafe", "'CometA'", GC9A01A_WHITE);
 }
 
 void loop() {
-  float rad = angle * PI / 180.0;
-  int x = CENTER_X + ORBIT_R * cos(rad);
-  int y = CENTER_Y + ORBIT_R * sin(rad);
-
-  // erase the oldest trail dot
-  tft.fillCircle(trailX[TRAIL_LEN - 1], trailY[TRAIL_LEN - 1], DOT_R, GC9A01A_BLACK);
-
-  // shift trail back by one
-  for (int i = TRAIL_LEN - 1; i > 0; i--) {
-    trailX[i] = trailX[i - 1];
-    trailY[i] = trailY[i - 1];
-  }
-
-  // insert new position at front
-  trailX[0] = x;
-  trailY[0] = y;
-
-  // draw trail from oldest to newest so newest is on top
-  for (int i = TRAIL_LEN - 1; i >= 0; i--) {
-    uint8_t brightness = (uint8_t)(255.0f * (TRAIL_LEN - i) / TRAIL_LEN);
-    uint16_t color = tft.color565(brightness, 0, 0);
-    tft.fillCircle(trailX[i], trailY[i], DOT_R, color);
-  }
-
-  angle += 3;
-  if (angle >= 360) angle = 0;
-
+  updateComet(comets[0]);
+  updateComet(comets[1]);
   delay(20);
 }
